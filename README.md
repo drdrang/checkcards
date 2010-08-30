6fe9ad15345d96d4ab59e443e649f1998bfdb00b
Dr. Drang <drdrang@gmail.com>
2010-08-30 13:58:33 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,30 @@
+Checkcards is set of scripts for querying the Naperville Public Library about the status of items checked out and on hold for all the members of a family. [This blog post][1] describes the motivation for the scripts and how they work.
+
+To use the scripts, you'll have to make these changes:
+
+* In `checkcards.py`, the section starting on Line 8 will have to be personalized with your email addresses and library card information.
+
+    * `mailFrom` is the sender's email address
+    * `mailTo` is the recipient's email address; it can be the same as the sender's
+    * `cardList` is a list of Python dictionary entries, each of which consists of
+    
+        * `patron`: the cardholder's name, which can be a first name of a nickname—it isn't used to log in.
+        * `code`: the barcode number on the front of the library card
+        * `pin`: the cardholder's PIN
+    
+    I keep my settings in a file called `personal.py`, which isn't included in the repository.
+    
+* In `checkcards`, Line 3 needs the full path to `checkcards.py`.
+
+* In `com.leancrew.checkcards.plist`, the ProgramArguments entry needs the full path to `checkcards`, and the StartCalendarInterval entry should be set to whenever you want the program to run.
+
+The `com.leancrew.checkcards.plist` file is needed if you plan to use OS X's `launchd` system to run `checkcards` at scheduled times. Put it in your `~/Library/LaunchAgents/` folder (which you may need to create) and run
+
+    launchctl load ~/Library/LaunchAgents/com.leancrew.checkcards.plist
+
+from the Terminal. You can check that it loaded correctly by running
+
+    launchctl list | grep leancrew
+
+
+[1]: http://www.leancrew.com/all-this/2009/03/library-loan-tracking-again/
---
+++ checkcards.py
@@ -0,0 +1,176 @@
+#!/usr/bin/python
+
+import mechanize
+from BeautifulSoup import BeautifulSoup
+from time import strptime, strftime, localtime
+import re
+
+# Card and email information. Uncomment and change this to suit your situation.
+# The "code" entry in each cardList dictionary is the patron's library barcode.
+# mailFrom = 'someone@example.com'
+# mailTo = 'someone@example.com'
+# cardList = [
+# {'patron' : 'Mom', 'code' : '12345678901234', 'pin' : '1234'},
+# {'patron' : 'Dad', 'code' : '98765432109876', 'pin' : '9876'},
+# {'patron' : 'Kid', 'code' : '45678912345678', 'pin' : '4567'}]
+
+# The URLs for the library's account information.
+# Login
+lURL = 'https://encore.naperville-lib.org:443/iii/cas/login?service=https%3A%2F%2Flibrary.naperville-lib.org%3A443%2Fpatroninfo~S1%2FIIITICKET&scope=1'
+# Checked-out items
+cURL = 'https://library.naperville-lib.org:443/patroninfo~S1/1110947/items'
+# On-hold items
+hURL = 'https://library.naperville-lib.org:443/patroninfo~S1/1110947/holds'
+
+# Initialize the lists of checked-out and on-hold items.
+checkedOut = []
+onHold = []
+
+# Function that returns an HTML table row for checked out items.
+def cRow(data):
+  if data[0][0:3] <= localtime()[0:3]: # due today or earlier
+    classString = ' class="due"'
+  else:
+    classString = ''
+  return '''<tr%s><td>%s</td><td>%s</td><td>%s</td></tr>''' % \
+  (classString, strftime('%b %d', data[0]), data[2], data[1])
+
+# Function that returns an HTML table row for items on hold.
+def hRow(data):
+  if data[0] <= 0:  # Waiting for pickup or in transit
+    classString = ' class="due"'
+  else:
+    classString = ''
+  return '''<tr%s><td>%s</td><td>%s</td><td>%s</td></tr>''' % \
+  (classString, data[3], data[2], data[1])
+
+# Go through each card, collecting the lists of items.
+for card in cardList:
+  # Need to use cookies to retain the session after login.
+  cookies = mechanize.CookieJar()
+  opener = mechanize.build_opener(mechanize.HTTPCookieProcessor(cookies))
+  mechanize.install_opener(opener)
+  
+  # Login
+  br = mechanize.Browser()
+  br.set_handle_robots(False)
+  br.open(lURL)
+  br.select_form(nr=0)    # the login form is the first on the page
+  br['code'] = card['code']
+  br['pin'] = card['pin']
+  resp = br.submit()
+  
+  # Get the pages.
+  cHtml = br.open(cURL).read()
+  hHtml = br.open(hURL).read()
+  
+  # Parse the HTML.
+  cSoup = BeautifulSoup(cHtml)
+  hSoup = BeautifulSoup(hHtml)
+
+  # Collect the table rows that contain the items.
+  loans = cSoup.findAll('tr', {'class' : 'patFuncEntry'})
+  holds = hSoup.findAll('tr', {'class' : 'patFuncEntry'})
+  
+  # Due dates and pickup dates are of the form mm-dd-yy.
+  dueDate = re.compile(r'\d\d-\d\d-\d\d')
+  
+  # Go through each row of checked out items, keeping only the title and due date.
+  for item in loans:
+    # The title is everything before the spaced slash in the patFuncTitle
+    # string. Some titles have a patFuncVol span after the title string;
+    # that gets filtered out by contents[0].
+    title = item.find('td', {'class' : 'patFuncTitle'}).a.contents[0].split(' / ')[0].strip()
+    
+    # The due date is somewhere in the patFuncStatus cell.
+    dueString = dueDate.findall(item.find('td', {'class' : 'patFuncStatus'}).contents[0])[0]
+    due = strptime(dueString, '%m-%d-%y')
+    # Add the item to the checked out list. Arrange tuple so items
+    # get sorted by due date.
+    checkedOut.append((due, card['patron'], title))
+  
+  # Go through each row of holds, keeping only the title and place in line.
+  for item in holds:
+    # Again, the title is everything before the spaced slash.
+    title = item.find('td', {'class' : 'patFuncTitle'}).a.string.split(' / ')[0].strip()
+    # The book's status in the hold queue will be either:
+    # 1. 'n of m holds'
+    # 2. 'Ready. Must be picked up by mm-dd-yy'
+    # 3. 'IN TRANSIT'
+    status = item.find('td', {'class' : 'patFuncStatus'}).contents[0].strip()
+    n = status.split()[0]
+    if n.isdigit():                         # possibility 1
+      n = int(n)
+      status = status.replace(' holds', '')
+    elif n[:5] == 'Ready':                  # possibility 2
+      n = -1
+      dueString = dueDate.findall(status)[0]
+      status = 'Ready<br/> ' + strftime('%b %d', strptime(dueString, '%m-%d-%y'))
+    else:                                   # possibility 3
+      n = 0
+    # Add the item to the on hold list. Arrange tuple so items
+    # get sorted by position in queue. The position is faked for
+    # items ready for pickup and in transit within the library. 
+    onHold.append((n, card['patron'], title, status))
+
+# Sort the lists.
+checkedOut.sort()
+onHold.sort()
+
+# Templates for the email.
+mailHeader = '''From: %s
+To: %s
+Subject: Library items
+Content-Type: text/html
+'''
+
+pageHeader = '''<html>
+<head>
+<style type="text/css">
+body {
+  font-family: Helvetica, Sans-serif;
+}
+h1 {
+  font-size: 150%%;
+  margin-top: 1.5em;
+  margin-bottom: .25em;
+}
+table {
+  border-collapse: collapse; 
+}
+table th {
+  padding: .5em 1em .25em 1em;
+  background-color: #ddd;
+  border: 1px solid black;
+  border-bottom: 2px solid black;
+}
+table tr.due {
+  background-color: #fcc;
+}
+table td {
+  padding: .25em 1em .25em 1em;
+  border: 1px solid black;
+}
+</style>
+</head>
+<body>
+<p>Hours: Mon-Fri: 9am - 9pm;  Sat: 9am - 5pm;  Sun: 12pm - 5pm</p>
+<p>As of %s</p>
+'''
+
+tableTemplate = '''<h1>%s</h1>
+<table>
+<tr><th>%s</th><th>Title</th><th>Card</th></tr>
+%s
+</table>
+'''
+
+pageFooter = '''</body>
+</html>'''
+
+# Print out the email header and contents. This should be piped to sendmail.
+print mailHeader % (mailFrom, mailTo)
+print pageHeader % strftime('%I:%M %p on %b %d, %Y')
+print (tableTemplate % ('Checked out', 'Due', '\n'.join([cRow(x) for x in checkedOut]))).encode('utf8')
+print (tableTemplate % ('On hold', 'Status', '\n'.join([hRow(x) for x in onHold]))).encode('utf8')
+print pageFooter
